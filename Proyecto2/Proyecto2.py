35c41ce81a543d7f3c7090d0cc97c8823b1d7c72
Luisa2125 <luiyi2125@gmail.com>
2018-06-07 18:58:16 -0600

    Add files via upload

--- Proyecto2/Proyecto2.py
+++ Proyecto2/Proyecto2.py
@@ -165,7 +165,7 @@ done = False
 while not done:
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
     view = glm.lookAt(camera, glm.vec3(0,0,0), glm.vec3(0,1,0))
-    scene = pyassimp.load('./modelos/Nectarins-peaches_obj.obj')
+    scene = pyassimp.load('./modelos/escenaf.obj')
     glize(scene.rootnode,0,0,100)
 
     done = process_input()
